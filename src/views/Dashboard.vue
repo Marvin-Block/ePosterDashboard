85b0f1cf8eaa5e362452ac3fe748cec1954c3f69
mblock <marvin_block@kodi.de>
2021-05-17 14:37:25 +0200

    Charts entfernt und Stat-Cards angepasst

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -5,69 +5,86 @@
     tag="section"
   >
     <v-row>
-      <v-col cols="12">
-        <v-row>
-          <v-col
-            v-for="(chart, i) in charts"
-            :key="`chart-${i}`"
-            cols="12"
-            md="6"
-            lg="4"
-          >
-            <material-chart-card
-              :color="chart.color"
-              :data="chart.data"
-              :options="chart.options"
-              :responsive-options="chart.responsiveOptions"
-              :title="chart.title"
-              :type="chart.type"
-            >
-              <template #subtitle>
-                <div class="font-weight-light text--secondary">
-                  <div v-html="chart.subtitle" />
-                </div>
-              </template>
 
-              <template #actions>
-                <v-icon
-                  class="mr-1"
-                  small
-                >
-                  mdi-clock-outline
-                </v-icon>
-
-                <span
-                  class="text-caption grey--text font-weight-light"
-                  v-text="chart.time"
-                />
-              </template>
-            </material-chart-card>
-          </v-col>
-        </v-row>
-      </v-col>
-
+<!--      stat cards below-->
       <v-col
-        v-for="({ actionIcon, actionText, ...attrs }, i) in stats"
+        v-for="(stat, i) in stats"
         :key="i"
         cols="12"
         md="6"
         lg="3"
       >
-        <material-stat-card v-bind="attrs">
-          <template #actions>
-            <v-icon
-              class="mr-2"
-              small
-              v-text="actionIcon"
-            />
-            <div class="text-truncate">
-              {{ actionText }}
+        <v-card
+          class="v-card--material mt-4"
+        >
+          <v-card-title class="align-start">
+            <v-sheet
+              :color="stat.color"
+              class="overflow-hidden mt-n9 transition-swing v-card--material__sheet"
+              elevation="6"
+              max-width="100%"
+              rounded
+            >
+              <v-theme-provider
+                dark
+              >
+                <div
+                  class="pa-7"
+                >
+                  <v-icon
+                    large
+                    v-text="stat.icon"
+                  />
+                </div>
+
+                <div
+                  class="text-h4 white--text pa-7 v-card--material__title"
+                >
+                  header
+                </div>
+              </v-theme-provider>
+            </v-sheet>
+
+            <div
+              class="text-h4 pl-4 v-card--material__title"
+            >
+              <template>
+                {{ stat.title }}
+              </template>
+
+              <div class="text-subtitle-1 mb-n2">
+                <template>
+                  <div
+                    class="text-right text-h3 v-card__subtitle--material-stats"
+                    v-text="stat.value"
+                  />
+                </template>
+              </div>
             </div>
+          </v-card-title>
+          <template>
+            <v-divider class="mt-2 mx-4" />
+
+            <v-card-actions class="px-4 text-caption grey--text">
+              <v-icon
+                class="mr-2"
+                small
+                v-text="stat.actionIcon"
+              />
+              <div
+                class="text-truncate"
+                v-text="stat.actionText"
+              >
+              </div>
+            </v-card-actions>
           </template>
-        </material-stat-card>
+        </v-card>
       </v-col>
 
+<!--      Spacer-->
       <v-col cols="12" />
+
+<!--      new videos-->
       <v-col
         cols="12"
         md="6"
@@ -80,7 +97,7 @@
         >
           <v-card-text>
             <v-data-table
-              :headers="headers"
+              :headers="videoHeaders"
               :items="this.videos.slice(0,5)"
               no-data-text="Es scheint keine neuen Videos zu geben"
               loading-text="Videos werden geladen..."
@@ -100,6 +117,8 @@
           </v-card-text>
         </material-card>
       </v-col>
+
+<!--      new devices-->
       <v-col
         cols="12"
         md="6"
@@ -112,10 +131,25 @@
         >
           <v-card-text>
             <v-data-table
+              :headers="deviceHeaders"
+              :items="this.devices.slice(0,5)"
+              :loading="this.devices.length < 1"
+              disable-sort
+              sort-by="createdAt"
+              sort-desc
               no-data-text="Es scheint keine neuen Geräte zu geben"
               loading-text="Geräte werden geladen..."
               hide-default-footer
-            />
+            >
+              <template v-slot:item.createdAt="{ item }">
+                <p>{{ formatTime(item.createdAt, "DD.MM.YYYY kk:mm" ) }}</p>
+              </template>
+              <template v-slot:item.lastRequest="{ item }">
+                <v-icon :color="item.lastRequest > (new Date).valueOf() - 300000 ? 'success' : 'error'">
+                  mdi-{{ item.lastRequest > (new Date).valueOf() - 300000 ? 'wifi' : 'wifi-off' }}
+                </v-icon>
+              </template>
+            </v-data-table>
           </v-card-text>
         </material-card>
       </v-col>
@@ -126,38 +160,17 @@
 <script>
   // Utilities
   import { get, sync } from 'vuex-pathify'
-  import Vue from 'vue'
   import moment from 'moment'
-
-  const lineSmooth = Vue.chartist.Interpolation.cardinal({
-    tension: 0,
-  })
 
   export default {
     name: 'DashboardView',
 
     data: () => ({
-      headers: [
-        // {
-        //   text: 'ID',
-        //   value: 'id',
-        // },
-        {
-          text: 'KW',
-          value: 'calendarWeek',
-        },
+      videoHeaders: [
         {
           text: 'Name',
           value: 'name',
         },
-        // {
-        //   text: 'Ausrichtung',
-        //   value: 'orientation_V2',
-        // },
-        // {
-        //   text: 'Rotation',
-        //   value: 'rotation',
-        // },
         {
           text: 'Größe',
           value: 'size',
@@ -166,171 +179,88 @@
           text: 'Erstellt',
           value: 'createdAt',
         },
-        // {
-        //   text: 'Verlinkungen',
-        //   value: 'link.length',
-        // },
-        // {
-        //   sortable: false,
-        //   text: '',
-        //   value: 'actions',
-        // },
       ],
-      countryData: {
-        US: 2920,
-        DE: 1390,
-        AU: 760,
-        GB: 690,
-        RO: 600,
-        BR: 550,
-      },
-      charts: [{
-        type: 'Bar',
-        color: 'primary',
-        title: 'Website Views',
-        subtitle: 'Last Campaign Performance',
-        time: 'updated 10 minutes ago',
-        data: {
-          labels: ['Ja', 'Fe', 'Ma', 'Ap', 'Mai', 'Ju', 'Jul', 'Au', 'Se', 'Oc', 'No', 'De'],
-          series: [
-            [542, 443, 320, 780, 553, 453, 326, 434, 568, 610, 756, 895],
-          ],
-        },
-        options: {
-          axisX: {
-            showGrid: false,
-          },
-          low: 0,
-          high: 1000,
-          chartPadding: {
-            top: 0,
-            right: 5,
-            bottom: 0,
-            left: 0,
-          },
-        },
-        responsiveOptions: [
-          ['screen and (max-width: 640px)', {
-            seriesBarDistance: 5,
-            axisX: {
-              labelInterpolationFnc: function (value) {
-                return value[0]
-              },
-            },
-          }],
-        ],
-      }, {
-        type: 'Line',
-        color: 'success',
-        title: 'Daily Sales',
-        subtitle: '<i class="mdi mdi-arrow-up green--text"></i><span class="green--text">55%</span>&nbsp;increase in today\'s sales',
-        time: 'updated 4 minutes ago',
-        data: {
-          labels: ['12am', '3pm', '6pm', '9pm', '12pm', '3am', '6am', '9am'],
-          series: [
-            [230, 750, 450, 300, 280, 240, 200, 190],
-          ],
-        },
-        options: {
-          lineSmooth,
-          low: 0,
-          high: 1000, // creative tim: we recommend you to set the high sa the biggest value + something for a better look
-          chartPadding: {
-            top: 0,
-            right: 0,
-            bottom: 0,
-            left: 0,
-          },
-        },
-      }, {
-        type: 'Line',
-        color: '#254AA5',
-        title: 'Completed Tasks',
-        subtitle: 'Last Campaign Performance',
-        time: 'campaign sent 26 minutes ago',
-        data: {
-          labels: ['M', 'T', 'W', 'T', 'F', 'S', 'S'],
-          series: [
-            [12, 17, 7, 17, 23, 18, 38],
-          ],
-        },
-        options: {
-          lineSmooth,
-          low: 0,
-          high: 50, // creative tim: we recommend you to set the high sa the biggest value + something for a better look
-          chartPadding: {
-            top: 0,
-            right: 0,
-            bottom: 0,
-            left: 0,
-          },
-        },
-      }],
-      listings: [
+      deviceHeaders: [
         {
-          image: 2,
-          location: 'Barcelona, Spain',
-          price: '$899/night',
-          text: 'The place is close to Barceloneta Beach and bus stop just 2 min by walk and near to "Naviglio" where you can enjoy the life in Barcelona.',
-          title: 'Cozy 5 Stars Apartment',
+          text: 'Standort',
+          value: 'location',
         },
         {
-          image: 3,
-          location: 'Office Studio',
-          price: '$1.119/night',
-          text: 'The place is close to Metro Station and bus stop just 2 min by walk and near to "Naviglio" where you can enjoy the night life in London, UK.',
-          title: 'Office Studio',
+          text: 'Beschreibung',
+          value: 'description',
         },
         {
-          image: 1,
-          location: 'Milan, Italy',
-          price: '$459/night',
-          text: 'The place is close to Metro Station and bus stop just 2 min by walk and near to "Naviglio" where you can enjoy the main night life in Milan.',
-          title: 'Beautiful Castle',
+          text: 'Status',
+          value: 'lastRequest',
+        },
+        {
+          text: 'Erstellt',
+          value: 'createdAt',
         },
       ],
       stats: [
         {
-          actionIcon: 'mdi-alert',
-          actionText: 'Get More Space...',
-          color: '#FD9A13',
-          icon: 'mdi-sofa-single',
-          title: 'Bookings',
-          value: '184',
+          actionIcon: 'mdi-calendar-range',
+          actionText: `Heute - ${moment(new Date()).format('DD.MM.YYYY')}`,
+          color: 'primary',
+          icon: 'mdi-creation',
+          title: 'Neue Videos',
+          value: '0',
         },
         {
-          actionIcon: 'mdi-tag',
-          actionText: 'Tracked from Google Analytics',
-          color: 'primary',
-          icon: 'mdi-chart-bar',
-          title: 'Website Visits',
-          value: '75.521',
+          // actionIcon: 'mdi-history',
+          actionText: '-',
+          color: 'success',
+          icon: 'mdi-wifi',
+          title: 'Geräte Online',
+          value: '215',
+        },
+        {
+          // actionIcon: 'mdi-history',
+          actionText: '-',
+          color: 'error',
+          icon: 'mdi-wifi-off',
+          title: 'Geräte Offline',
+          value: '5',
         },
         {
           actionIcon: 'mdi-calendar-range',
-          actionText: 'Last 24 Hours',
-          color: 'success',
-          icon: 'mdi-store',
-          title: 'Revenue',
-          value: '$34,245',
-        },
-        {
-          actionIcon: 'mdi-history',
-          actionText: 'Just Updated',
+          actionText: `Heute - ${moment(new Date()).format('DD.MM.YYYY')}`,
           color: '#254AA5',
-          icon: 'mdi-twitter',
-          title: 'Followers',
-          value: '+245',
+          icon: 'mdi-monitor-clean',
+          title: 'Neue Geräte',
+          value: '2',
         },
+        // {
+        //   actionIcon: 'mdi-tag',
+        //   actionText: 'Tracked from Google Analytics',
+        //   color: 'primary',
+        //   icon: 'mdi-chart-bar',
+        //   title: 'Website Visits',
+        //   value: '75.521',
+        // },
+        // {
+        //   actionIcon: 'mdi-calendar-range',
+        //   actionText: 'Last 24 Hours',
+        //   color: 'success',
+        //   icon: 'mdi-store',
+        //   title: 'Revenue',
+        //   value: '$34,245',
+        // },
+        // {
+        //   actionIcon: 'mdi-history',
+        //   actionText: 'Just Updated',
+        //   color: '#254AA5',
+        //   icon: 'mdi-twitter',
+        //   title: 'Followers',
+        //   value: '+245',
+        // },
       ],
     }),
     computed: {
       videos: get('app/videos'),
       devices: get('app/devices'),
       sales: get('sales/sales'),
-      totalSales () {
-        return this.sales.reduce((acc, val) => acc + val.salesInM, 0)
-      },
     },
     methods: {
       formatTime: function (time, format) {
@@ -345,3 +275,19 @@
     },
   }
 </script>
+<style lang="sass">
+.v-card.v-card--material
+  > .v-card__title
+    > .v-card--material__title
+      flex: 1 1 auto
+      word-break: break-word
+
+.v-card--material-stats.v-card--material .v-card--material__title
+  color: #999999
+  font-size: .875rem !important
+  margin-left: auto
+  text-align: right
+
+.v-card__subtitle--material-stats
+  color: #3C4858
+</style>
