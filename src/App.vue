4befbab23a89e8c5b9068b63e5c99d3550b893e4
mblock <marvin_block@kodi.de>
2021-06-08 13:04:38 +0200

    Memoryleak fix

--- src/App.vue
+++ src/App.vue
@@ -5,6 +5,7 @@
 </template>
 
 <script>
+  /* eslint-disable no-prototype-builtins */
   // Styles
   import '@/styles/overrides.sass'
   import { sync } from 'vuex-pathify'
@@ -72,34 +73,69 @@
           this.client.send('video')
           this.client.send('link')
         }
-      }, 2000)
+      }, 1000)
     },
     methods: {
       createWebsocket: function () {
         this.client = new WebSocket('ws://kodizabbix:3331')
         this.client.addEventListener('error', () => {
           this.networkStatus = this.statusList[1]
-          setTimeout(() => { this.createWebsocket() }, 2000)
+          setTimeout(() => { this.createWebsocket() }, 1000)
         })
         this.client.addEventListener('message', (e) => {
           const response = JSON.parse(e.data)
           if (response.type === 'device') {
-            if (response.rows.length !== this.devices.length) { this.devices = response.rows }
+            if (!this.object_equals(this.devices, response.rows)) { this.devices = response.rows }
           }
           if (response.type === 'video') {
-            if (response.rows.length !== this.videos.length) { this.videos = response.rows }
+            if (!this.object_equals(this.videos, response.rows)) { this.videos = response.rows }
           }
           if (response.type === 'link') {
-            // this.links = response.rows
+            if (!this.object_equals(this.links, response.rows)) { this.links = response.rows }
           }
         })
         this.client.addEventListener('close', () => {
           this.networkStatus = this.statusList[1]
-          setTimeout(() => { this.createWebsocket() }, 2000)
+          setTimeout(() => { this.createWebsocket() }, 1000)
         })
         this.client.addEventListener('open', () => {
           this.networkStatus = this.statusList[0]
         })
+      },
+      object_equals: function (x, y) {
+        let p
+        if (x === y) return true
+        // if both x and y are null or undefined and exactly the same
+
+        if (!(x instanceof Object) || !(y instanceof Object)) return false
+        // if they are not strictly equal, they both need to be Objects
+
+        if (x.constructor !== y.constructor) return false
+        // they must have the exact same prototype chain, the closest we can do is
+        // test there constructor.
+
+        for (p in x) {
+          if (!x.hasOwnProperty(p)) continue
+          // other properties were tested using x.constructor === y.constructor
+
+          if (!y.hasOwnProperty(p)) return false
+          // allows to compare x[ p ] and y[ p ] when set to undefined
+
+          if (x[p] === y[p]) continue
+          // if they have the same strict value or identity then they are equal
+
+          if (typeof (x[p]) !== 'object') return false
+          // Numbers, Strings, Functions, Booleans must be strictly equal
+
+          if (!this.object_equals(x[p], y[p])) return false
+          // Objects and Arrays must be tested recursively
+        }
+
+        for (p in y) {
+          if (y.hasOwnProperty(p) && !x.hasOwnProperty(p)) { return false }
+        }
+        // allows x[ p ] to be set to undefined
+        return true
       },
     },
   }
